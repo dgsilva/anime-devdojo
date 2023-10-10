00bdff6d36f7da8af91a0b0c15bd67091c4c05a6
Diego Bizerra da Silva <diegorj93@gmail.com>
2023-10-10 15:53:31 -0300

    first commit

---
+++ src/main/java/com/api/anime/SpringbootAnimeApplication.java
@@ -0,0 +1,13 @@
+package com.api.anime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAnimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootAnimeApplication.class, args);
+	}
+
+}
